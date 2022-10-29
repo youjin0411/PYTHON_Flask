099eb6316a42b2cf557437d26ca9eae9c9dc83ba
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-10-29 21:17:00 +0900

    프로젝트 초기화

--- TestProject/home/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path # path 함수를 이용하기 위해서 선언해줍니다.
-from . import views     # from 옆에 .(점)은 현재 폴더(app)를 의미합니다. 즉 현재 폴더에 views.py를 가져옵니다
-
-urlpatterns = [
-        path('',views.home, name='home'),
-]
--- TestProject/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(requeset):
-    return render(requeset, 'home.html')
