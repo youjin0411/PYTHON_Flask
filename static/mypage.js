8c1ac20b56f76a97d371827683a84ba6b4cad07d
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-11-25 00:08:50 +0900

    마이페이지

--- static/mypage.js
+++ static/mypage.js
@@ -1,11 +1,18 @@
 const character = document.getElementsByClassName("char");
 const back = document.getElementsByClassName("back");
-alert(character[0].innerHTML)
+const backgroundImage = 0;
 if (character[0].innerHTML == '병아리') {
-    // figure의 background-image를 images 폴더의 병아리.png로 변경
-    back.style.backgroundImage = url('../static/images/병아리.png');
+    back[0].style.backgroundImage = "url('../static/images/병아리.png')";
 }
 if (character[0].innerHTML == '고양이') {
-    // figure의 background-image를 images 폴더의 고양이.png로 변경
-    back.style.backgroundImage = url('../static/images/고양이.jpg');
+    back[0].style.backgroundImage = "url('../static/images/고양이.jpg')";
+}
+if (character[0].innerHTML == '호랑이') {
+    back[0].style.backgroundImage = "url('../static/images/호랑이.jpeg')";
+}
+if (character[0].innerHTML == '강아지') {
+    back[0].style.backgroundImage = "url('../static/images/강아지.jpg')";
+}
+if (character[0].innerHTML == '공작새') {
+    back[0].style.backgroundImage = "url('../static/images/공작새.jpg')";
 }
